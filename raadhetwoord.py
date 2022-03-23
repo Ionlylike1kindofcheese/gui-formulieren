8b716904b52f9502ab30b3667612d4c55e38cf3b
Ionlylike1kindofcheese <99066333@mydavinci.nl>
2022-03-23 10:06:56 +0100

    Dit hoort bij opdracht: F1.8.03.O3 - Raad het woord (On hold vanwege preselect letter in spinboxs die het niet doen ondanks dezelfde stappen)

--- raadhetwoord.py
+++ raadhetwoord.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 import tkinter as tk
+import string
 
 def caps(event):
     entryword.set(entryword.get().upper())
@@ -15,7 +17,44 @@ def wordcontrol():
     elif len(check) < 4:
         messagebox.showerror('Foutmelding','Het ingevulde woord is te klein!!!')
     else:
-        ...
+        player2(check)
+
+
+def player2(player1word):
+    window1.destroy()
+    window2 = tk.Tk()
+    window2.title("Raad het woord - Player 2")
+    window2.geometry("350x300")
+    window2.config(bg='white')
+    window2.resizable(False, False)
+    wordlabel = tk.Label(window2, text="Raad het woord:", font=('Helvetica', 19), bg='white')
+    wordlabel.pack(ipadx=10, ipady=10, fill='x')
+    spinboxgenerate(window2, player1word)
+
+    window2.mainloop()
+
+
+def spinboxgenerate(window2, chosenword):
+    alphabet = list(string.ascii_uppercase)
+    lengthWord = len(chosenword)
+    valueList = []
+    widthfill = 7
+    gapfill = 40
+    timesnumber = 70
+    for adjustment in range(4,8):
+        if lengthWord == adjustment:
+            break
+        else:
+            widthfill -= 1
+            gapfill -= 10
+            timesnumber -= 10
+    for times in range(lengthWord):
+        valueList.append(chosenword[times])
+        var = StringVar(window2)
+        var.set(valueList[times])
+        spin_box = ttk.Spinbox(window2, values=alphabet, textvariable=var, wrap=True, state='readonly', width=widthfill)
+        spin_box.place(y=150, x=(gapfill+(times*timesnumber)))
+
 
 window1 = tk.Tk()
 window1.title("Raad het woord - Player 1")
